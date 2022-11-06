ea61ac3dbe19fcb69a81aaed08dff1f7eeec4c5e
Abhijit Gupta <avgupta456@gmail.com>
2022-11-06 15:14:07 -0500

    EmbeddingExplainer

---
+++ src/explainers/embedding.py
@@ -0,0 +1,38 @@
+import torch
+from torch_geometric.utils import get_num_hops
+
+from src.explainers.explainer import Explainer
+from src.utils import get_neighbors_single_node, sigmoid
+
+
+class EmbeddingExplainer(Explainer):
+    def __init__(self, pred_model, x, edge_index):
+        super().__init__(pred_model, x, edge_index)
+
+        self.num_hops = get_num_hops(pred_model)
+
+    def explain_edge(self, node_idx_1, node_idx_2):
+        # TODO: Can use k_hop_subgraph here, not urgent
+        x, edge_index = self.x, self.edge_index
+
+        neighbors_1 = get_neighbors_single_node(edge_index, node_idx_1)
+        label_index = torch.tensor([[node_idx_2 for _ in neighbors_1], neighbors_1])
+        label_index = label_index.to(x.device)
+        node_idx_1_sim = self.pred_model(x, edge_index, label_index)[1]
+
+        neighbors_2 = get_neighbors_single_node(edge_index, node_idx_2)
+        label_index = torch.tensor([[node_idx_1 for _ in neighbors_2], neighbors_2])
+        label_index = label_index.to(x.device)
+        node_idx_2_sim = self.pred_model(x, edge_index, label_index)[1]
+
+        output = {}
+        for i, neighbor in enumerate(neighbors_1):
+            output[neighbor] = sigmoid(node_idx_1_sim[i])
+        for i, neighbor in enumerate(neighbors_2):
+            new = sigmoid(node_idx_2_sim[i])
+            if neighbor in output:
+                old = output[neighbor]
+                output[neighbor] = max(old, new)
+            else:
+                output[neighbor] = new
+        return output
