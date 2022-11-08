cca1b17a75d635033783e0f239b7f3fd233887fb
Abhijit Gupta <avgupta456@gmail.com>
2022-11-08 15:58:24 -0500

    Update data storage

--- src/experiments/vary_sparsity/base.py
+++ src/experiments/vary_sparsity/base.py
@@ -13,9 +13,8 @@ from src.explainers.main import (
     sample_random,
     sample_subgraphx,
 )
-from src.metrics.fidelity import charact_prob, fid_minus_prob, fid_plus_prob
 from src.pred import Net
-from src.utils import get_neighbors, sigmoid
+from src.utils import get_neighbors
 
 
 def run_experiment(
@@ -28,8 +27,8 @@ def run_experiment(
         n_neighbors = get_neighbors(edge_index, node_idx_1, node_idx_2).shape[0]
         print(i, node_idx_1, node_idx_2, n_neighbors)
 
-        curr_edge_label_index = torch.tensor([[node_idx_1], [node_idx_2]]).to(device)
-        initial_pred = model(x, edge_index, curr_edge_label_index)[1]
+        _label_index = torch.tensor([[node_idx_1], [node_idx_2]]).to(device)
+        initial_pred = model(x, edge_index, _label_index)[1]
 
         sampler_outputs = []
         for sampler, sampler_name in zip(samplers, sampler_names):
@@ -39,8 +38,6 @@ def run_experiment(
 
         sub_edge_mask = (edge_index[1] == node_idx_1) | (edge_index[1] == node_idx_2)
 
-        sufficient_results = defaultdict(list)
-        necessary_results = defaultdict(list)
         results = defaultdict(list)
         for k in range(n_neighbors + 1):
             for output, sampler_name in zip(sampler_outputs, sampler_names):
@@ -50,44 +47,36 @@ def run_experiment(
                 S_filter[sub_edge_mask] = 0
                 S_filter[sub_edge_mask & keep_edges] = 1
 
-                expl_pred = model(x, edge_index[:, S_filter], curr_edge_label_index)[1]
-                fid_minus = fid_minus_prob(sigmoid(initial_pred), sigmoid(expl_pred))
-                sufficient_results[sampler_name].append(1 - fid_minus.item())
+                expl_pred = model(x, edge_index[:, S_filter], _label_index)[1]
 
                 S_filter = np.ones(edge_index.shape[1], dtype=bool)
                 S_filter[sub_edge_mask] = 1
                 S_filter[sub_edge_mask & keep_edges] = 0
 
-                remove_pred = model(x, edge_index[:, S_filter], curr_edge_label_index)[
-                    1
-                ]
-                fid_plus = fid_plus_prob(sigmoid(initial_pred), sigmoid(remove_pred))
-                necessary_results[sampler_name].append(fid_plus.item())
+                remove_pred = model(x, edge_index[:, S_filter], _label_index)[1]
 
-                charact = charact_prob(
-                    sigmoid(initial_pred), sigmoid(expl_pred), sigmoid(remove_pred)
+                new_node = None if k == 0 else int(output[k - 1][0])
+                results[sampler_name].append(
+                    [
+                        new_node,
+                        float(initial_pred.item()),
+                        float(expl_pred.item()),
+                        float(remove_pred.item()),
+                    ]
                 )
-                results[sampler_name].append(charact.item())
+
+        all_results[i] = results
 
         if show_plots:
-            for result, result_label in zip(
-                [sufficient_results, necessary_results, results],
-                ["Sufficient", "Necessary", "Characterization"],
-            ):
-                fig, ax = plt.subplots()
-                for sampler_name in sampler_names:
-                    ax.plot(result[sampler_name], label=sampler_name)
-                ax.set_xlabel("Number of nodes")
-                ax.set_ylabel("Prediction")
-                ax.set_title(f"{result_label} vs Sparsity")
-                ax.legend()
-                plt.show()
-
-        all_results[i] = {
-            "sufficient": sufficient_results,
-            "necessary": necessary_results,
-            "characterization": results,
-        }
+            fig, ax = plt.subplots()
+            for sampler_name in sampler_names:
+                temp_data = [x[2] for x in results[sampler_name]]
+                ax.plot(temp_data, label=sampler_name)
+            ax.set_xlabel("Number of nodes")
+            ax.set_ylabel("Prediction")
+            ax.set_title("Explanation Prediction vs Sparsity")
+            ax.legend()
+            plt.show()
 
     return all_results
 
--- src/experiments/vary_sparsity/run_multiple.py
+++ src/experiments/vary_sparsity/run_multiple.py
@@ -2,27 +2,27 @@ import json
 
 import torch
 
+from src.dataset import dataset, device, test_data
+from src.experiments.vary_sparsity.base import run_experiment
+from src.explainers.main import (
+    sample_degree,
+    sample_edge_subgraphx,
+    sample_embedding,
+    sample_gnnexplainer,
+    sample_random,
+    sample_subgraphx,
+)
+from src.pred import Net
 
-def get_data():
-    from src.dataset import dataset, device, test_data
-    from src.experiments.vary_sparsity.base import (
-        run_experiment,
-        sample_degree,
-        sample_edge_subgraphx,
-        sample_embedding,
-        sample_gnnexplainer,
-        sample_random,
-        sample_subgraphx,
-    )
-    from src.pred import Net
 
+def get_data(start, stop):
     model = Net(dataset.num_features, 128, 32).to(device)
     model.load_state_dict(torch.load("./models/model.pt"))
     all_data = run_experiment(
         model,
         test_data.x,
         test_data.edge_index,
-        test_data.edge_label_index[:, 0:600],
+        test_data.edge_label_index[:, start : stop + 1],
         [
             sample_gnnexplainer,
             sample_subgraphx,
@@ -37,6 +37,7 @@ def get_data():
 
 
 if __name__ == "__main__":
-    all_data = get_data()
-    with open("./results/vary_sparsity/data_600.json", "w") as f:
+    start, stop = 0, 600
+    all_data = get_data(start, stop)
+    with open(f"./results/vary_sparsity/data_{start}_{stop}.json", "w") as f:
         json.dump(all_data, f)
