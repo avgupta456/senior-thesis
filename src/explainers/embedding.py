ded4c1ff7bacd4f86a1d747507f861b3c1f17d79
Abhijit Gupta <avgupta456@gmail.com>
2022-12-04 16:00:12 -0500

    Redo Embedding method
    Much much better

--- src/explainers/embedding.py
+++ src/explainers/embedding.py
@@ -5,7 +5,6 @@ from torch_geometric.utils import get_num_hops
 
 from src.explainers.explainer import Explainer
 from src.utils.neighbors import get_neighbors
-from src.utils.utils import device, sigmoid
 
 
 class EmbeddingExplainer(Explainer):
@@ -22,28 +21,29 @@ class EmbeddingExplainer(Explainer):
     def explain_edge(self, data, node_idx_1, node_1_type, node_idx_2, node_2_type):
         output = defaultdict(int)
         z_dict = self.pred_model.encode(data.x_dict, data.edge_index_dict)
-        for n_idx, n_type in [(node_idx_1, node_1_type), (node_idx_2, node_2_type)]:
-            neighbors_1 = get_neighbors(data, n_idx, n_type, 0, "")
-            for k, v in neighbors_1.items():
-                if len(v) == 0:
-                    continue
 
-                if (k, "to", n_type) in data.edge_index_dict:
-                    edge_label_index = [v, [n_idx] * len(v)]
-                    edge_key = (k, "to", n_type)
-                elif (n_type, "to", k) in data.edge_index_dict:
-                    edge_label_index = [[n_idx] * len(v), v]
-                    edge_key = (n_type, "to", k)
-                else:
-                    continue
+        # Node 1 neighbors
+        neighbors_1 = get_neighbors(data, node_idx_1, node_1_type, 0, "")
+        for k, v in neighbors_1.items():
+            if len(v) == 0:
+                continue
 
-                edge_label_index = torch.tensor(edge_label_index).to(device)
-                sim = self.pred_model.decode(
-                    z_dict[edge_key[0]], z_dict[edge_key[2]], edge_label_index
-                )
+            n2_z = z_dict[node_2_type][node_idx_2]
+            for neighbor in v:
+                neighbor_z = z_dict[k][neighbor]
+                sim = torch.cosine_similarity(n2_z, neighbor_z, dim=0)
+                output[(k, neighbor)] = max(output[(k, neighbor)], sim)
 
-                for i, neighbor in enumerate(v):
-                    value = sigmoid(sim[i]).item()
-                    output[(k, neighbor)] = max(output[(k, neighbor)], value)
+        # Node 2 neighbors
+        neighbors_2 = get_neighbors(data, node_idx_2, node_2_type, 0, "")
+        for k, v in neighbors_2.items():
+            if len(v) == 0:
+                continue
+
+            n1_z = z_dict[node_1_type][node_idx_1]
+            for neighbor in v:
+                neighbor_z = z_dict[k][neighbor]
+                sim = torch.cosine_similarity(n1_z, neighbor_z, dim=0)
+                output[(k, neighbor)] = max(output[(k, neighbor)], sim)
 
         return output
